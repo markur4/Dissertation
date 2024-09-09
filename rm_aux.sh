5102480547726ed64c83e8090106756338297b69
markur <98951648+markur4@users.noreply.github.com>
2024-09-09 16:39:56 +0200

    small details

--- rm_aux.sh
+++ rm_aux.sh
@@ -7,3 +7,4 @@ rm -f THESIS/PARTS/*.aux
 rm -f THESIS/*.aux THESIS/*.bbl THESIS/*.blg THESIS/*.fdb_latexmk THESIS/*.fls THESIS/*.log THESIS/*.out THESIS/*.synctex.gz THESIS/*.toc THESIS/*.xdv
 rm -f THESIS/.aux
 rm -f THESIS/*.pdf
+rm -f THESIS/*.ent
